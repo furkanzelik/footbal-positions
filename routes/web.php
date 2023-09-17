d70cdfca40e4ecfd13dc18d28fd1af5f0be5e1be
husey <furaknzelik@gmail.com>
2023-09-17 16:03:03 +0200

    Nog steeds een 500 server fout, routes niet goed gekoppeld?

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/home',function (){
+
+    return view('home');
+
+    Route::get('home',HomeController::class,'index');
 });
